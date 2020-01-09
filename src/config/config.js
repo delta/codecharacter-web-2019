5021b8e03ff7ef10cb623f7afdbc4098fdb24cfe
Chethan2k1 <40890937+Chethan2k1@users.noreply.github.com>
2020-01-09 19:13:53 +0530

    Squash some commits

---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+export const API_BASE_URL = 'http://localhost:4000/';
+export const SOCKET_BASE_URL = 'http://localhost:4000/';
+export const RECAPTCHA_SITE_KEY = '6LcXvJAUAAAAAGh6lIUJMROvK7baFjtL739mIN6U';
+export const RECAPTCHA_SECRET_KEY = '6LcXvJAUAAAAANU5jI8u24A3WTwhHIOs-fHgFuVm';
